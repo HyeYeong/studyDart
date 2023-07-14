cf109183901c02f25c91455c41a4ab5b1fe35f2f
hye-yeong lee <i-e@yukoyuko.co.jp>
2023-07-14 17:22:00 +0900

    feat: 1-2変数の定義方法

--- main.dart
+++ main.dart
@@ -1,5 +1,8 @@
 void main() {
   print('hello, world!');
+  var name = 'HELLO'; // ←こちがおすすめ。後で型定義はできる。データのタイプを守るなら、アップデートも可能
+  String name2 = 'HEELLO';
+
   for (int i = 0; i < 5; i++) {
     print('hello ${i + 1}');
   }
