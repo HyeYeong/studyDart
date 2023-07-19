1caf7abc882106d3d6bb8f20587fc6ac70687e38
hye-yeong lee <i-e@yukoyuko.co.jp>
2023-07-19 13:52:43 +0900

    commit

--- main.dart
+++ main.dart
@@ -1,6 +1,18 @@
 void main() {
   print('hello, world!');
   var name = 'HELLO'; // ←こちがおすすめ。後で型定義はできる。データのタイプを守るなら、アップデートも可能
+  var name1; //dynamic type 形がどんな種類かわからない場合
+  name1 = 1;
+  name1 = true;
+  // でも、タイプを指定すると、Dartはそのタイプにより使えるMethodを自動に見せてくる
+  dynamic name3; //理想は、使わないこと
+  if (name3 is String) {
+    // Check type
+  }
+  if (name3 is int) {
+    // Check type
+  }
+
   String name2 = 'HEELLO';
 
   for (int i = 0; i < 5; i++) {
